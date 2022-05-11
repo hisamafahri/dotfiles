9f4d6dc2e207ff5402a1a976ccc96131241768a2
Hisam Fahri <me@hisamafahri.com>
2022-05-11 22:36:46 +0700

    feat(nvim): recreating the nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,31 +1,73 @@
-local present, impatient = pcall(require, "impatient")
+local Plug = vim.fn['plug#']
+local set = vim.opt
+local key = vim.keymap
 
-if present then
-   impatient.enable_profile()
-end
+-- PLUG SERIES__________
 
-local core_modules = {
-   "core.utils",
-   "core.options",
-   "core.autocmds",
-   "core.mappings",
-}
+vim.call('plug#begin', '~/.config/nvim/plugged')
+Plug 'Mofiqul/dracula.nvim'
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim'
+Plug('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+vim.call('plug#end')
 
-for _, module in ipairs(core_modules) do
-   local ok, err = pcall(require, module)
-   if not ok then
-      error("Error loading " .. module .. "\n\n" .. err)
-   end
-end
+-- COLORSCHEME SERIES__________
+vim.cmd "colorscheme dracula"
 
--- check if custom init.lua file exists
-if vim.fn.filereadable(vim.fn.stdpath "config" .. "/lua/custom/init.lua") == 1 then
-   -- try to call custom init, if not successful, show error
-   local ok, err = pcall(require, "custom")
+-- SET SERIES__________
 
-   if not ok then
-      vim.notify("Error loading custom/init.lua\n\n" .. err)
-   end
+-- tabs/indentaion
+set.tabstop = 4
+set.softtabstop = 4
+set.shiftwidth = 4
+set.expandtab = true
+set.smartindent = true
 
-   return
-end
+-- number
+set.relativenumber = true
+set.number = true
+
+-- search
+set.hlsearch = false
+set.incsearch = true
+
+-- files/buffer
+set.hidden = true
+set.undofile = true
+set.undodir = "~/.nvim/undodir"
+set.backup = false
+set.swapfile = false
+
+-- window
+set.splitbelow = true
+set.splitright = true
+set.scrolloff = 8
+set.sidescrolloff = 8
+set.wrap = false
+set.showmode = false
+set.mouse = "n"
+set.termguicolors = true
+set.background = "dark"
+-- set.signcolumn = "yes"
+
+-- input/completion
+set.completeopt = "menuone,noinsert,noselect"
+
+
+-- KEYMAPPINGS SET__________
+vim.g.mapleader = ' '
+
+-- Telescope
+key.set('n', '<Leader>ff', ':Telescope find_files hidden=true<CR>')
+key.set('n', '<Leader>fg', ':Telescope live_grep<CR>')
+key.set('n', '<Leader>fb', ':Telescope buffers<CR>')
+
+-- Split
+key.set('n', '<Leader>vn', ':vnew <CR>')
+key.set('n', '<Leader>hn', ':new <CR>')
+key.set('n', '<Leader>v', ':vsplit <CR>')
+key.set('n', '<Leader>h', ':split <CR>')
+
+
+-- todo
+-- 1) completion
