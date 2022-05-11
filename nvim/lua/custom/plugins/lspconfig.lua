8e91ff851080d77cb948f061db2cb2e32e54eb65
Hisam Fahri <me@hisamafahri.com>
2022-05-11 19:51:11 +0700

    fix(nvim): remove unnecessary files

---
+++ nvim/lua/custom/plugins/lspconfig.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+M.setup_lsp = function(attach, capabilities)
+   local lspconfig = require "lspconfig"
+
+   -- lspservers with default config
+   local servers = { "html", "cssls", "rust_analyzer", "eslint" }
+
+   for _, lsp in ipairs(servers) do
+      lspconfig[lsp].setup {
+         on_attach = attach,
+         capabilities = capabilities,
+      }
+   end
+end
+
+return M
+
