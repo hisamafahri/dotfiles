f37546aa7103cf2ef41a040f8d482064e0eca13e
Hisam Fahri <me@hisamafahri.com>
2022-07-06 12:36:27 +0700

    fix(nvim): show hidden file on telescope

--- nvim/lua/settings/plugins/telescope.lua
+++ nvim/lua/settings/plugins/telescope.lua
@@ -1,11 +1,16 @@
 require('telescope').setup{
     defaults = {
         file_ignore_patterns = {
-            ".git/", 
-            "node_modules/", 
-            ".svelte-kit/", 
-            "package/", 
+            ".git/",
+            "node_modules/",
+            ".svelte-kit/",
+            "package/",
             "build/"
+        }
+    },
+    pickers = {
+        find_files = {
+            hidden = true
         }
     }
 }
