6a4a726ae5ea400d8afabe10e89a70b82e16facc
Hisam Fahri <me@hisamafahri.com>
2022-12-17 23:34:58 +0700

    feat(config): cleanup old mess

--- nvim/lua/colors/init.lua
+++
@@ -1,8 +0,0 @@
-local cmd = vim.cmd
-local set = vim.opt
-
-set.termguicolors = true -- Enable/disable 24-bit colors
-
--- cmd 'colorscheme challenger_deep'
-cmd 'colorscheme deus'
--- cmd 'colorscheme gruvbox'
--- nvim/lua/settings/plugins/luasnip.lua
+++
@@ -1,8 +0,0 @@
-require 'luasnip'.snippets = {
-    all = {
-        -- Snippets that available in any filetype
-    },
-    lua = {
-        -- Snippets that available in lua
-    },
-}
