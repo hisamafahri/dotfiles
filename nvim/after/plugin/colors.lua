5010fa687fa8edbaa10a9ae5b3dfd7cccd95eac1
Hisam Fahri <me@hisamafahri.com>
2022-12-19 10:28:25 +0700

    feat(nvim): change some things

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,3 +1,5 @@
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
 
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
