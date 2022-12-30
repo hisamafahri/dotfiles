a796e4b207bedf17f7bf6da9c9381e5737ba6b5e
Hisam Fahri <hisam.fahri@pitik.id>
2022-12-30 13:09:32 +0700

    feat(nvim): add `nightowl` theme

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -145,6 +145,11 @@ _G.packer_plugins = {
     path = "/Users/hisamafahri/.local/share/nvim/site/pack/packer/start/neoformat",
     url = "https://github.com/sbdchd/neoformat"
   },
+  ["night-owl.vim"] = {
+    loaded = true,
+    path = "/Users/hisamafahri/.local/share/nvim/site/pack/packer/start/night-owl.vim",
+    url = "https://github.com/haishanh/night-owl.vim"
+  },
   ["nvim-autopairs"] = {
     config = { "\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0" },
     loaded = true,
