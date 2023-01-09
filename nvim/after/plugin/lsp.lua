e0797978268931e569df55a36cdb6ac5c291ac85
Hisam Fahri <hisam.fahri@pitik.id>
2023-01-09 07:41:57 +0700

    feat(nvim): defined `vim` as global variable

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,12 +1,25 @@
 local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
+
+-- Fix Undefined global 'vim'
+lsp.configure('sumneko_lua', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
+
 lsp.ensure_installed({
   'tsserver',
   'eslint',
   'sumneko_lua'
 })
 lsp.setup()
+
 
 vim.diagnostic.config({
   virtual_text = true,
