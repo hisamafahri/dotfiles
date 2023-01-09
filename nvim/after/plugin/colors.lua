eac8d3b816777169651d2516ed96fe83bd4d9c37
Hisam Fahri <hisam.fahri@pitik.id>
2023-01-09 07:42:15 +0700

    feat(nvim): add `rose-pine` colorscheme

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,8 +1,9 @@
 vim.o.background = "dark" -- or "light" for light mode
 vim.o.termguicolors = true
 
-vim.cmd([[colorscheme rose-pine]])
+-- vim.cmd([[colorscheme rose-pine]])
 -- vim.cmd([[colorscheme night-owl]])
+vim.cmd([[colorscheme gruvbox]])
 -- let g:lightline = { 'colorscheme': 'nightowl' }
 
 -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
