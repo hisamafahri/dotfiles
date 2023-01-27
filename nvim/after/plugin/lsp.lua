077f66001cfec7623ade77a3346bbf38d5926bbf
Hisam Fahri <hisam.fahri@pitik.id>
2023-01-27 19:48:14 +0700

    feat(nvim):32;2uimprove autocomplete

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -40,6 +40,23 @@ lsp.ensure_installed({
     'eslint',
     'sumneko_lua'
 })
+
+local cmp = require('cmp')
+
+lsp.setup_nvim_cmp({
+    mapping = cmp.mapping.preset.insert({
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+    })
+})
+
 lsp.setup()
 
 
