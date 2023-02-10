bf13d1efe65c668d61d7146eb13059c5692642cf
Hisam Fahri <hisam.fahri@pitik.id>
2023-02-10 19:37:27 +0700

    fix(lsp): fix completion shortcut

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,5 +1,11 @@
 local lsp = require('lsp-zero')
-local cmp = require('cmp')
+local ok_cmp, cmp = pcall(require, 'cmp')
+local select_opts = {behavior = 'select'}
+local s = {}
+
+if ok_cmp then
+  select_opts = {behavior = cmp.SelectBehavior.Select}
+end
 
 lsp.preset('recommended')
 
@@ -36,17 +42,63 @@ lsp.configure('tailwindcss', {
     },
 })
 
+require'lspconfig'.dartls.setup{}
+
 lsp.ensure_installed({
     'tsserver',
     'eslint',
-    'sumneko_lua'
+    'sumneko_lua',
 })
 
 lsp.setup_nvim_cmp({
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
-    })
+    }),
+  mapping = cmp.mapping.preset.insert({
+    ['<C-Space>'] = cmp.mapping.complete(),
+    -- confirm selection
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+    ['<C-y>'] = cmp.mapping.confirm({select = false}),
+
+    -- navigate items on the list
+    ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
+    ['<Down>'] = cmp.mapping.select_next_item(select_opts),
+    ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
+    ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
+
+    -- toggle completion
+    ['<C-e>'] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.abort()
+      else
+        cmp.complete()
+      end
+    end),
+
+    -- when menu is visible, navigate to next item
+    -- when line is empty, insert a tab character
+    -- else, activate completion
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item(select_opts)
+      elseif s.check_back_space() then
+        fallback()
+      else
+        cmp.complete()
+      end
+    end, {'i', 's'}),
+
+    -- when menu is visible, navigate to previous item on list
+    -- else, revert to default behavior
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item(select_opts)
+      else
+        fallback()
+      end
+    end, {'i', 's'}),
+  })
 })
 
 lsp.setup()
