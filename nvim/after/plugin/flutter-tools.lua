33866379ac58e264ebcd048a79226a064598e693
Hisam Fahri <hisam.fahri@pitik.id>
2023-02-18 22:22:11 +0700

    fix(nvim): fix `flutter-tools` issues

--- nvim/after/plugin/flutter-tools.lua
+++ nvim/after/plugin/flutter-tools.lua
@@ -1,9 +1,3 @@
-require("flutter-tools").setup {
-  flutter_path = "/opt/homebrew/bin/flutter",
-  dev_log = {
-    enabled = true,
-    open_cmd = "tabedit", -- command to use to open the log buffer
-  },
-}
+require("flutter-tools").setup {}
 
 
