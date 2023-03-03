f611988cd5de530a19f546fedd7ab915229894bf
Hisam Fahri <hisam.fahri@pitik.id>
2023-03-03 15:06:02 +0700

    feat(nvim): update and fix colors

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -11,6 +11,6 @@ vim.cmd([[colorscheme catppuccin]])
 -- vim.g.lightline = { colorscheme = 'rose-pine' }
 vim.g.lightline = { colorscheme = 'catppuccin' }
 
--- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
