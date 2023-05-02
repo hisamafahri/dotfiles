9e8a02bded0774961bc240ed72c169741ecb04b7
Hisam Fahri <hisam.fahri@pitik.id>
2023-05-02 19:27:06 +0700

    feat(configs): update a lot of things

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -3,14 +3,14 @@ vim.opt.termguicolors = true
 -- Themes
 vim.opt.background = "dark"
 -- vim.o.background = "light"
+vim.g.gruvbox_material_background = 'hard'
 
 -- vim.cmd([[colorscheme catppuccin]])
 -- vim.cmd([[colorscheme catppuccin-latte]])
-vim.cmd([[colorscheme gruvbox]])
+vim.cmd([[colorscheme gruvbox-material]])
 
 -- vim.g.lightline = { colorscheme = 'rose-pine' }
 -- vim.g.lightline = { colorscheme = 'catppuccin' }
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
