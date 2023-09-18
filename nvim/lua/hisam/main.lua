f9e4e40d7126b8d9657ac8a92babebb4c04bb897
Hisam Fahri <hisam.fahri@pitik.id>
2023-09-18 09:49:08 +0700

    feat(nvim, kitty): adjust auto dark-light mode switch

--- nvim/lua/hisam/main.lua
+++ nvim/lua/hisam/main.lua
@@ -28,10 +28,12 @@ return require("lazy").setup({
       set_dark_mode = function()
         vim.api.nvim_set_option("background", "dark")
         vim.cmd("colorscheme kanagawa")
+        vim.fn.system("kitty +kitten themes kanagawa")
       end,
       set_light_mode = function()
         vim.api.nvim_set_option("background", "light")
         vim.cmd("colorscheme kanagawa")
+        vim.fn.system("kitty +kitten themes kanagawa_light")
       end,
     },
     init = function()
