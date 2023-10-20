1d9dcbed47a066bb692b091c1be8b3d92438fb5b
Hisam Fahri <hisam.fahri@pitik.id>
2023-10-20 08:09:08 +0700

    fix(vim): adjust fold indicator and add max line in `treesitter-context`

--- nvim/after/plugin/general.lua
+++ nvim/after/plugin/general.lua
@@ -3,3 +3,8 @@ require("Comment").setup()
 require("nvim-ts-autotag").setup()
 require("pqf").setup()
 require("auto-dark-mode").init()
+require 'treesitter-context'.setup {
+  enable = true,           -- Enable this plugin (Can be enabled/disabled later via commands)
+  max_lines = 0,           -- How many lines the window should span. Values <= 0 mean no limit.
+  multiline_threshold = 1, -- Maximum number of lines to show for a single context
+}
