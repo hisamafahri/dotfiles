78205fbefd57c8cf1ed030e7ccbd8dcf14734c06
Hisam Fahri <hisam.fahri@pitik.id>
2023-10-24 06:51:52 +0700

    feat: add itemr gruvbox light, make nvim transparent

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -9,15 +9,11 @@ auto_dark_mode.setup({
   set_dark_mode = function()
     vim.api.nvim_set_option("background", "dark")
     vim.cmd("colorscheme gruvbox")
-    vim.fn.system("kitty +kitten themes Gruvbox Dark")
   end,
   set_light_mode = function()
-    -- vim.api.nvim_set_option("background", "light")
-    -- vim.cmd("colorscheme gruvbox")
     -- vim.fn.system("kitty +kitten themes Gruvbox Light")
     vim.api.nvim_set_option("background", "dark")
     vim.cmd("colorscheme gruvbox")
-    vim.fn.system("kitty +kitten themes Gruvbox Dark")
   end,
 })
 
@@ -27,11 +23,11 @@ vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
 
 -- ================ CONFLICT SETTINGS ================
-vim.cmd("highlight ConflictMarkerBegin guibg=#2f7366")
-vim.cmd("highlight ConflictMarkerOurs guibg=#2e5049")
-vim.cmd("highlight ConflictMarkerTheirs guibg=#344f69")
-vim.cmd("highlight ConflictMarkerEnd guibg=#2f628e")
-vim.cmd("highlight ConflictMarkerCommonAncestorsHunk guibg=#754a81")
+-- vim.cmd("highlight ConflictMarkerBegin guibg=#2f7366")
+-- vim.cmd("highlight ConflictMarkerOurs guibg=#2e5049")
+-- vim.cmd("highlight ConflictMarkerTheirs guibg=#344f69")
+-- vim.cmd("highlight ConflictMarkerEnd guibg=#2f628e")
+-- vim.cmd("highlight ConflictMarkerCommonAncestorsHunk guibg=#754a81")
 
 -- ================ DARK MODE SETTINGS ================
 -- -- Remove the 'fg' in the 'EndOfBuffer' to show the ~
