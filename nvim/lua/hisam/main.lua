de8eb18b0f508a9843f90cc04cf332024719dca6
Hisam Fahri <hisam.fahri@pitik.id>
2023-12-07 07:38:03 +0700

    fix(nvim): adjust for `LuaSnip` issue
    See: https://github.com/L3MON4D3/LuaSnip/issues/1071

--- nvim/lua/hisam/main.lua
+++ nvim/lua/hisam/main.lua
@@ -42,7 +42,13 @@ return require("lazy").setup({
       -- Autocompletion
       { "hrsh7th/nvim-cmp" },     -- Required
       { "hrsh7th/cmp-nvim-lsp" }, -- Required
-      { "L3MON4D3/LuaSnip" },     -- Required
+      {
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp"
+      }
     },
   },
   { "kevinhwang91/nvim-ufo",     dependencies = "kevinhwang91/promise-async" },
