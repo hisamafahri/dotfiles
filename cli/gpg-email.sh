c4edd7b7dd5dfa72762b565a7be144ddf6dd9268
Hisam Fahri <hisam.fahri@pitik.id>
2024-01-09 14:25:01 +0700

    feat(cli): add base gpg encryption script

---
+++ cli/gpg-email.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Send encrypted email
+# Requires gpg and mail to be setup
+echo "Available keys:"
+gpg --list-keys
+
+# Gather variables
+echo "Enter public key of recipient:"
+read -e user
+
+echo "Enter email:"
+read -e email
+
+echo "Enter subject:"
+read -e subject
+
+echo "Enter message:"
+read message
+
+# Pipe the echoed message to gpg, sign and encrypt it to ascii (-eas), include your key so you can read it,
+# include recipients key, pipe to mail with the (unencrypted) subject, send to the given email.
+# echo "$message" | gpg2 --no-emit-version -eas -r hisamafahri@gmail.com -r $user | mail -s "$subject" $email
+
+# Generate a timestamp for the filename
+timestamp=$(date +%Y%m%d%H%M%S)
+filename="output_${timestamp}.gpg"
+
+echo "$message" | gpg2 --no-emit-version -eas -r hisamafahri@gmail.com -r $user > "$filename"
