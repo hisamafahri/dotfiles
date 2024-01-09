f8b1bd16da59839ae7c752e226ac2b177f0823f3
Hisam Fahri <hisam.fahri@pitik.id>
2024-01-09 14:26:25 +0700

    fix(nvim): adjust missing `require()` parameters

--- nvim/lua/hisamafahri/init.lua
+++ nvim/lua/hisamafahri/init.lua
@@ -1,33 +1,19 @@
-require("hisamafahri/vim")
-
 local vim = vim
 
 vim.g.mapleader = " "
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-  { import = "hisamafahri.plugins.editor" },
-  { import = "hisamafahri.plugins.git" },
-  { import = "hisamafahri.plugins.language" },
-  { import = "hisamafahri.plugins.lsp" },
-  { import = "hisamafahri.plugins.telescope" },
-  { import = "hisamafahri.plugins.themes" },
-  { import = "hisamafahri.plugins.treesitter" },
-
-  -- Experiments
-  { dir = "~/projects/gdocs.nvim" },
-})
-
+-- Note: required first
+require("hisamafahri/lazy")
+require("hisamafahri/vim")
 require("hisamafahri/remap")
+
+require("hisamafahri.configs.bufferline")
+require("hisamafahri.configs.formatter")
+require("hisamafahri.configs.gitsigns")
+require("hisamafahri.configs.lsp")
+require("hisamafahri.configs.lualine")
+require("hisamafahri.configs.nvim-tree")
+require("hisamafahri.configs.telescope")
+require("hisamafahri.configs.treesitter")
+require("hisamafahri.configs.ufo")
+
---
+++ nvim/lua/hisamafahri/lazy.lua
@@ -0,0 +1,26 @@
+local vim = vim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  { import = "hisamafahri.plugins.editor" },
+  { import = "hisamafahri.plugins.git" },
+  { import = "hisamafahri.plugins.language" },
+  { import = "hisamafahri.plugins.lsp" },
+  { import = "hisamafahri.plugins.telescope" },
+  { import = "hisamafahri.plugins.themes" },
+  { import = "hisamafahri.plugins.treesitter" },
+
+  -- Experiments
+  { dir = "~/projects/gdocs.nvim" },
+})
