8c08b7f49d033d2fb7ab46f10c5ef3a1326d9106
Hisam Fahri <hisam.fahri@pitik.id>
2024-01-25 12:49:51 +0700

    feat: add `hop.nvim`

--- nvim/lua/hisamafahri/plugins/editor.lua
+++ nvim/lua/hisamafahri/plugins/editor.lua
@@ -47,19 +47,13 @@ return {
     end,
   },
 
-  -- Flash/jump
+  -- hop/jump
   {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    ---@type Flash.Config
+    "smoka7/hop.nvim",
+    version = "*",
     opts = {},
-    -- stylua: ignore
-    keys = {
-      { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-      { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-      { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-      { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
-    },
+    config = function()
+      require("hop").setup({})
+    end,
   },
 }
