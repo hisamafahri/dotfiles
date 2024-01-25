d1aeb8070ccc9728fbb8c3e5d4ac8b7ec2d7c434
Hisam Fahri <hisam.fahri@pitik.id>
2024-01-25 07:10:49 +0700

    feat: adjust themes

--- nvim/lua/hisamafahri/plugins/themes.lua
+++ nvim/lua/hisamafahri/plugins/themes.lua
@@ -1,7 +1,26 @@
 local vim = vim
 
 return {
-  { "sainnhe/gruvbox-material" },
+  -- Themes[0]: gruvbox-material
+  {
+    "sainnhe/gruvbox-material",
+    enabled = true,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_transparent_background = 0
+      vim.g.gruvbox_material_foreground = "mix"
+      vim.g.gruvbox_material_background = "hard"    -- soft, medium, hard
+      vim.g.gruvbox_material_ui_contrast = "high"   -- The contrast of line numbers, indent lines, etc.
+      vim.g.gruvbox_material_float_style = "bright" -- Background of floating windows
+      vim.g.gruvbox_material_statusline_style = "material"
+
+      -- vim.g.gruvbox_material_colors_override = { bg0 = '#000000' } -- #0e1010
+      -- vim.g.gruvbox_material_colors_override = { bg0 = "#121212" }
+      -- vim.g.gruvbox_material_better_performance = 1
+
+      vim.cmd.colorscheme("gruvbox-material")
+    end,
+  },
 
   -- Change Neovim theme automatically on system appearance change
   {
@@ -9,15 +28,10 @@ return {
     config = {
       update_interval = 1000,
       set_dark_mode = function()
-        vim.g.gruvbox_material_background = "hard"
         vim.api.nvim_set_option("background", "dark")
-        vim.cmd("colorscheme gruvbox-material")
       end,
       set_light_mode = function()
-        -- vim.api.nvim_set_option("background", "light")
         vim.api.nvim_set_option("background", "dark")
-        vim.g.gruvbox_material_background = "hard"
-        vim.cmd("colorscheme gruvbox-material")
       end,
     },
   },
