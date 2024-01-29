b1db123b06dee53ae4ec1938fce44bae71bab9a7
Hisam Fahri <hisam.fahri@pitik.id>
2024-01-29 15:17:08 +0700

    feat(nvim): adjust lsp configs

--- nvim/lua/hisamafahri/configs/lsp.lua
+++ nvim/lua/hisamafahri/configs/lsp.lua
@@ -1,17 +1,69 @@
-local lsp = require("lsp-zero").preset({})
+-- NOTE: references https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/configuration-templates.md#primes-config
+local lsp_zero = require("lsp-zero")
 
--- (Optional) Configure lua language server for neovim
-require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
+lsp_zero.on_attach(function(client, bufnr)
+  local opts = { buffer = bufnr, remap = false }
 
-lsp.setup()
+  vim.keymap.set("n", "gd", function()
+    vim.lsp.buf.definition()
+  end, opts)
+  vim.keymap.set("n", "K", function()
+    vim.lsp.buf.hover()
+  end, opts)
+  vim.keymap.set("n", "<leader>vws", function()
+    vim.lsp.buf.workspace_symbol()
+  end, opts)
+  vim.keymap.set("n", "<leader>vd", function()
+    vim.diagnostic.open_float()
+  end, opts)
+  vim.keymap.set("n", "[d", function()
+    vim.diagnostic.goto_next()
+  end, opts)
+  vim.keymap.set("n", "]d", function()
+    vim.diagnostic.goto_prev()
+  end, opts)
+  vim.keymap.set("n", "<leader>vca", function()
+    vim.lsp.buf.code_action()
+  end, opts)
+  vim.keymap.set("n", "<leader>vrr", function()
+    vim.lsp.buf.references()
+  end, opts)
+  vim.keymap.set("n", "<leader>vrn", function()
+    vim.lsp.buf.rename()
+  end, opts)
+  vim.keymap.set("i", "<C-h>", function()
+    vim.lsp.buf.signature_help()
+  end, opts)
+end)
 
--- You need to setup `cmp` after lsp-zero
+-- to learn how to use mason.nvim with lsp-zero
+-- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+require("mason").setup({})
+require("mason-lspconfig").setup({
+  ensure_installed = { "tsserver", "rust_analyzer" },
+  handlers = {
+    lsp_zero.default_setup,
+    lua_ls = function()
+      local lua_opts = lsp_zero.nvim_lua_ls()
+      require("lspconfig").lua_ls.setup(lua_opts)
+    end,
+  },
+})
+
 local cmp = require("cmp")
-
 local cmp_action = require("lsp-zero").cmp_action()
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 cmp.setup({
-  mapping = {
+  sources = {
+    { name = "path" },
+    { name = "nvim_lsp" },
+    { name = "nvim_lua" },
+    { name = "luasnip", keyword_length = 2 },
+    { name = "buffer",  keyword_length = 3 },
+  },
+  formatting = lsp_zero.cmp_format(),
+  mapping = cmp.mapping.preset.insert({
     -- `Enter` key to confirm completion
     ["<CR>"] = cmp.mapping.confirm({ select = false }),
 
@@ -23,8 +75,5 @@ cmp.setup({
     ["<C-b>"] = cmp_action.luasnip_jump_backward(),
     ["<Tab>"] = cmp_action.luasnip_supertab(),
     ["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
-  },
+  }),
 })
-
--- Optional: cmp for vim-dadbod
--- cmp.setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })
