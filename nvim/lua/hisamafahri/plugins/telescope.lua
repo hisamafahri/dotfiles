27fd099dbbc6275e68afc1c9fb00aa158cec12d3
Hisam Fahri <hisam.fahri@pitik.id>
2024-01-29 15:21:14 +0700

    feat(nvim): add telescope select

--- nvim/lua/hisamafahri/plugins/telescope.lua
+++ nvim/lua/hisamafahri/plugins/telescope.lua
@@ -1,8 +1,9 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    version = "*",
-    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-live-grep-args.nvim" },
-  },
-  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	{
+		"nvim-telescope/telescope.nvim",
+		version = "*",
+		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-live-grep-args.nvim" },
+	},
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	{ "nvim-telescope/telescope-ui-select.nvim" },
 }
