1e616c2555496310ab33f982f4dd0875dc5f355f
Hisam Fahri <hisam.fahri@pitik.id>
2024-01-30 09:50:32 +0700

    feat(nvim): regular update

--- nvim/lua/hisamafahri/configs/lsp.lua
+++ nvim/lua/hisamafahri/configs/lsp.lua
@@ -40,7 +40,7 @@ end)
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
 require("mason").setup({})
 require("mason-lspconfig").setup({
-  ensure_installed = { "tsserver", "rust_analyzer" },
+  ensure_installed = { "tsserver", "rust_analyzer", "gopls" },
   handlers = {
     lsp_zero.default_setup,
     lua_ls = function()
