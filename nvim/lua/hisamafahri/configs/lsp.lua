88e305f404c333b4a8151b7388437266edc590ed
Hisam Fahri <hisam.fahri@pitik.id>
2024-02-06 07:19:58 +0700

    refactor(nvim): migrate away from `lsp-zero`

--- nvim/lua/hisamafahri/configs/lsp.lua
+++ nvim/lua/hisamafahri/configs/lsp.lua
@@ -1,22 +1,63 @@
+local vim = vim
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  desc = "LSP actions",
+  callback = function(event)
+    local opts = { buffer = event.buf }
+
+    -- these will be buffer-local keybindings
+    -- because they only work if you have an active language server
+
+    vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+    vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+    vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+    vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+    vim.keymap.set(
+      "n",
+      "go",
+      "<cmd>lua vim.lsp.buf.type_definition()<cr>",
+      opts
+    )
+    vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+    vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+    vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+    vim.keymap.set(
+      { "n", "x" },
+      "<F3>",
+      "<cmd>lua vim.lsp.buf.format({async = true})<cr>",
+      opts
+    )
+    vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+  end,
+})
+
+local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+local default_setup = function(server)
+  require("lspconfig")[server].setup({
+    capabilities = lsp_capabilities,
+  })
+end
+
+require("mason").setup({})
+require("mason-lspconfig").setup({
+  ensure_installed = { "tsserver", "rust_analyzer", "gopls" },
+  handlers = {
+    default_setup,
+  },
+})
+
 local cmp = require("cmp")
-local lsp_zero = require("lsp-zero")
-local lspconfig = require("lspconfig")
-
-local lsp = lsp_zero.preset({})
-local cmp_action = lsp_zero.cmp_action()
-
-lsp.setup()
-lspconfig.lua_ls.setup({})
+local luasnip = require("luasnip")
 
 cmp.setup({
-  -- sources = {
-  --   { name = "nvim_lsp" },
-  -- },
-  -- snippet = {
-  --   expand = function(args)
-  --     require("luasnip").lsp_expand(args.body)
-  --   end,
-  -- },
+  sources = {
+    -- { name = "path" },
+    { name = "nvim_lsp" },
+    { name = "nvim_lua" },
+    { name = "luasnip", keyword_length = 2 },
+    { name = "buffer", keyword_length = 3 },
+  },
   mapping = cmp.mapping.preset.insert({
     -- `Enter` key to confirm completion
     ["<CR>"] = cmp.mapping.confirm({ select = false }),
@@ -25,9 +66,32 @@ cmp.setup({
     ["<C-Space>"] = cmp.mapping.complete(),
 
     -- Navigate between snippet placeholder
-    ["<C-f>"] = cmp_action.luasnip_jump_forward(),
-    ["<C-b>"] = cmp_action.luasnip_jump_backward(),
-    ["<Tab>"] = cmp_action.luasnip_supertab(),
-    ["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+        -- this will auto complete if our cursor in next to a word and we press tab
+        -- elseif has_words_before() then
+        --     cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
   }),
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
 })
--- nvim/lua/hisamafahri/plugins/lsp.lua
+++ nvim/lua/hisamafahri/plugins/lsp.lua
@@ -1,31 +1,28 @@
 local vim = vim
 
 return {
+  -- LSP Support
+  { "neovim/nvim-lspconfig" }, -- Required
   {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v2.x",
-    dependencies = {
-      -- LSP Support
-      { "neovim/nvim-lspconfig" }, -- Required
-      {
-        -- Autoinstall LSP
-        "williamboman/mason.nvim",
-        build = function()
-          pcall(vim.cmd, "MasonUpdate")
-        end,
-      },
-      { "williamboman/mason-lspconfig.nvim" }, -- Optional
+    -- Autoinstall LSP
+    "williamboman/mason.nvim",
+    build = function()
+      pcall(vim.cmd, "MasonUpdate")
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "neovim/nvim-lspconfig" },
+  },
 
-      -- Autocompletion
-      { "hrsh7th/nvim-cmp" }, -- Required
-      { "hrsh7th/cmp-nvim-lsp" }, -- Required
-      {
-        "L3MON4D3/LuaSnip",
-        -- follow latest release.
-        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-        -- install jsregexp (optional!).
-        build = "make install_jsregexp",
-      },
-    },
+  -- Autocompletion
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  {
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "v2.*",
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp",
   },
 }
