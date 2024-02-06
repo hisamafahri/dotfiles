6caaadea3921082567f411a85db946c36c54a8e8
Hisam Fahri <hisam.fahri@pitik.id>
2024-02-06 10:49:17 +0700

    fix(nvim): adjust lsp keymap

--- nvim/lua/hisamafahri/configs/lsp.lua
+++ nvim/lua/hisamafahri/configs/lsp.lua
@@ -8,26 +8,28 @@ vim.api.nvim_create_autocmd("LspAttach", {
     -- these will be buffer-local keybindings
     -- because they only work if you have an active language server
 
-    vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-    vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-    vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-    vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
-    vim.keymap.set(
-      "n",
-      "go",
-      "<cmd>lua vim.lsp.buf.type_definition()<cr>",
-      opts
-    )
-    vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
-    vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
-    vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-    vim.keymap.set(
-      { "n", "x" },
-      "<F3>",
-      "<cmd>lua vim.lsp.buf.format({async = true})<cr>",
-      opts
-    )
-    vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
+
+    -- NOTE: Rarely used
+    -- vim.keymap.set(
+    --   "n",
+    --   "go",
+    --   "<cmd>lua vim.lsp.buf.type_definition()<cr>",
+    --   opts
+    -- )
+    -- vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+    -- vim.keymap.set(
+    --   { "n", "x" },
+    --   "<F3>",
+    --   "<cmd>lua vim.lsp.buf.format({async = true})<cr>",
+    --   opts
+    -- )
   end,
 })
 
