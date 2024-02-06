832e007926f6468064e421c2ff48d9b949cece0a
Hisam Fahri <hisam.fahri@pitik.id>
2024-02-06 07:26:04 +0700

    chore(nvim): stylua

--- nvim/lua/hisamafahri/plugins/themes.lua
+++ nvim/lua/hisamafahri/plugins/themes.lua
@@ -25,7 +25,7 @@ return {
     config = function()
       require("transparent").setup({
         extra_groups = {
-          "NormalFloat",    -- plugins which have float panel such as Lazy, Mason, LspInfo
+          "NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
           "FloatBorder",
           "NvimTreeNormal", -- NvimTree
         },
--- nvim/lua/hisamafahri/remap.lua
+++ nvim/lua/hisamafahri/remap.lua
@@ -107,7 +107,7 @@ vim.keymap.set(
 vim.keymap.set("n", "<leader><leader>", builtin.buffers, {})
 
 vim.o.foldcolumn = "1" -- '0' is not bad
-vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
