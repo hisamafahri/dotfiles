d147ae010b4b9dce57bc8e0b73a22659a40eee14
Hisam Fahri <hisam.fahri@pitik.id>
2024-02-07 22:03:06 +0700

    refactor(nvim): refactor formatter

---
+++ nvim/lua/hisamafahri/plugins/formatter.lua
@@ -0,0 +1,40 @@
+-- Ref: https://github.com/josean-dev/dev-environment-files/blob/e2ccfd444918260e39927a48ca9edb02939bf433/.config/nvim/lua/josean/plugins/formatting.lua#L2
+return {
+  "stevearc/conform.nvim",
+  lazy = true,
+  event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        svelte = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        graphql = { "prettier" },
+        lua = { "stylua" },
+        python = { "isort", "black" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      },
+    })
+
+    vim.keymap.set({ "n", "v" }, "Ï", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      })
+    end, { desc = "Format file or range (in visual mode)" })
+  end,
+}
