7dcea7e472a8612d38673edef8d12612afd8d16d
Hisam Fahri <me@hisamafahri.com>
2024-02-25 17:04:28 +0700

    feat(zsh): adjust cycle trough history bindings

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -2,3 +2,12 @@
 bindkey "\u001B[1;5C" forward-word
 # Step 1 word backward
 bindkey "\u001B[1;5D" backward-word
+
+# Cycle through history
+# Ref: https://superuser.com/a/585004
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
