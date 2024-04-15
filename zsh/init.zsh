5537d9ece5c38d5e58c2df024f8c87c43d4e2cd7
Hisam Fahri <me@hisamafahri.com>
2024-04-15 07:13:33 +0700

    fix(terminal): fix jump home & end line

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -3,6 +3,11 @@ bindkey "\u001B[1;5C" forward-word
 # Step 1 word backward
 bindkey "\u001B[1;5D" backward-word
 
+# Jump home/end
+# Ref: https://stackoverflow.com/a/33049500
+bindkey  "^[[1~"   beginning-of-line
+bindkey  "^[[4~"   end-of-line
+
 # Cycle through history
 # Ref: https://superuser.com/a/585004
 autoload -U up-line-or-beginning-search
