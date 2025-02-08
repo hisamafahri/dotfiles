b16a9a70703274a587567ffd1c9f2d5136e1335b
Hisam Fahri <me@hisamafahri.com>
2025-02-08 20:20:18 +0700

    feat: blink

---
+++ nvim/lua/packages/blink.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "saghen/blink.cmp",
+    dependencies = "rafamadriz/friendly-snippets",
+    version = "*",
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' for mappings similar to built-in completion
+      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+      -- See the full "keymap" documentation for information on defining your own keymap.
+      keymap = { preset = "default" },
+
+      appearance = {
+        kind_icons = {},
+      },
+    },
+  },
+}
