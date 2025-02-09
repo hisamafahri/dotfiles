dc3d979db079098f6dce5c6ca006578c473baa5f
Hisam Fahri <me@hisamafahri.com>
2025-02-09 15:53:30 +0700

    feat(nvim): completion

--- nvim/lua/packages/lspconfig.lua
+++ nvim/lua/packages/lspconfig.lua
@@ -15,10 +15,7 @@ return {
       { "j-hui/fidget.nvim", opts = {} },
 
       -- Allows extra capabilities provided by nvim-cmp
-      {
-        "hrsh7th/cmp-nvim-lsp",
-        dependencies = { "hrsh7th/nvim-cmp" },
-      },
+      { "hrsh7th/cmp-nvim-lsp" },
     },
     config = config.lspconfig,
   },
