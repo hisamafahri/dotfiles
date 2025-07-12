61b6fd0984b168a6ad3fff250ebe0ef58baa8ebd
Hisam Fahri <iam@hisamafahri.com>
2025-07-12 20:11:35 +0700

    fix(zed): space leader delay

--- install.sh
+++ install.sh
@@ -100,6 +100,7 @@ skhd --start-service
 yabai --start-service
 
 # Make key repeat rate faster
+defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 # Note: only applied after re-login
