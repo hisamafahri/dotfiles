174f29b6605b1c4208d186ad50976bde481c9f34
Hisam Fahri <iam@hisamafahri.com>
2025-09-02 09:45:07 +0700

    chore(nvim): lualine

--- nvim/lua/config/editor.lua
+++ nvim/lua/config/editor.lua
@@ -24,6 +24,15 @@ require("lualine").setup({
     icons_enabled = true,
     theme = "auto",
   },
+  sections = {
+    lualine_c = {
+      {
+        "filename",
+        file_status = true,
+        path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+      },
+    },
+  },
 })
 
 require("orgmode").setup({
